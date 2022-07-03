7ccad84026b5989251f40f61ed7704b1bd6bc9f9
Carlos José Cavalcante Junior <carlosjosecjr@outlook.com>
2022-07-03 11:51:21 -0300

    movendo componente 'content' para Content.tsx

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,48 @@
-export function Content() {
+import { MovieCard } from "./MovieCard";
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface ContentProps {
+  headerTitle: string
+  moviesContent: MovieProps[]
+}
+
+interface HeaderContentPros {
+  title: string
+}
+
+function HeaderContent(props: HeaderContentPros) {
+  return (
+    <header>
+      <span className="category">Categoria:<span>{props.title}</span></span>
+    </header>
+  )
+}
+
+export function Content(props: ContentProps) {
   // Complete aqui
+
+  return (
+    <div className="container">
+
+      <HeaderContent title={props.headerTitle}/>
+
+      <main>
+        <div className="movies-list">
+          {props.moviesContent.map(movie => (
+            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
